e014ef33d92549fb77a5337533cd2777a2dd0a39
unknown <ciglesiasvera@gmail.com>
2024-11-10 13:39:52 -0300

    Crud

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n&!&-wo1jzxk*=gir-o8&xzzzyz*$w3r&d0o*20%17_024@8u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['desplegar-projecto.onrender.com','localhost']
+ALLOWED_HOSTS = ['desplegar-projecto.onrender.com','127.0.0.1']
 
 
 # Application definition
